392e9bec17b8a5ee24d4b78e17a0b73ad748073d
Sage Raflik <neonfuz@gmail.com>
2021-02-26 17:05:20 -0600

    slippi-bin: WIP: init at 2.3.1
    still needs .desktop file & touchups

---
+++ pkgs/misc/emulators/slippi-bin/default.nix
@@ -0,0 +1,26 @@
+{ appimageTools, fetchurl, lib, glib-networking }:
+
+let
+  pname = "slippi-bin";
+  version="2.3.1";
+in
+appimageTools.wrapType2 rec {
+  name = "${pname}-${version}";
+
+  extraPkgs = pkgs: with pkgs; [ gmp webkitgtk ];
+
+  src = fetchurl {
+    url = "https://github.com/project-slippi/Ishiiruka/releases/download/v${version}/Slippi_Online-x86_64.AppImage";
+    sha256 = "0zz7zvnjp5s14wj4cri8ppkdvkiv095jpgql27xbmbzyx915qia4";
+  };
+
+  profile = "export GIO_EXTRA_MODULES=${glib-networking}/lib/gio/modules";
+
+  meta = with lib; {
+    homepage = "https://slippi.gg/netplay";
+    description = "Dolphin emulator customized for online melee netplay"; # TODO: improve
+    platforms = [ "x86_64-linux" ];
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ neonfuz ];
+  };
+}
